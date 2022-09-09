7cc148deac21f926fbe08bf8b72284b8a6540ff9
Rashard <rashadabeid@gmai.com>
2022-09-09 13:09:24 -0700

    hello world

---
+++ 0x00-hello_world/5-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Print a text
+ *
+ * Return:Always 0 (success)
+ */
+int main(void)
+{
+	prinf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
