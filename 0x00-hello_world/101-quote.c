2a7e924e3dce44c166b4179ed76e64f34cc69172
Rashard <rashadabeid@gmai.com>
2022-09-09 14:39:44 -0700

    hello C world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main- Entry point of a program
+ *
+ * Return: 1 on success
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\"- Dora Krpar,2015-1019\n",59);
+	return (1);
+}
