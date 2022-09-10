6262cd3493e5701c42c067882c20243ec60a636f
Rashard <rashadabeid@gmai.com>
2022-09-10 01:16:43 -0700

    hello C world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,8 +16,9 @@ int main(void)
 			{
 				putchar((i / 10) + '0');
 				putchar((i % 10) + '0');
-				putchar(' ');						putchar((p / 10) + '0');
-						putchar((p % 10) + '0');
+				putchar(' ');
+				putchar((p / 10) + '0');
+				putchar((p % 10) + '0');
 						if (i != 98)
 						{
 						putchar(',');
