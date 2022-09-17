b2a9fa6bc64b0b9d6d13923953295371081c5fc2
Rashard <rashadabeid@gmai.com>
2022-09-17 09:22:37 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,12 +13,11 @@ int i;
 printif("Infinite loop iincming :(\n");
 
 i = 0;
-/*
-*while (i > 10)
-*{
+
+while (i > 10)
+{
 *putchar(1);
-*}
-*/
+}
 printif("Infinite loop avoided! \\o/\n");
 
 return (0);
