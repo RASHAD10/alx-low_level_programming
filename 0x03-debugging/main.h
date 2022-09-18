a1dd44a53d7da064a8c71bc04dcbf95ba99f6193
Rashard Abeid <34215626+RASHAD10@users.noreply.github.com>
2022-09-18 13:52:41 +0300

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,3 +1,6 @@
 #include <stdio.h>
 
-void print_remaining_days(int a, int b, int c);
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int day, int month, int year);
