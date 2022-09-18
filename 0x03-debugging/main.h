c35496718a252f794cb747c8521ca2eb33de4333
Rashard Abeid <34215626+RASHAD10@users.noreply.github.com>
2022-09-18 17:55:39 +0300

    Delete main.h

--- 0x03-debugging/main.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
-void print_remaining_days(int day, int month, int year);
